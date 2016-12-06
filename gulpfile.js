677526cd24170fba9274fa958a9d1df4377c2548
chenag <23110134@qq.com>
2016-12-07 01:38:16 +0800

    m

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+/**
+ * Created by cag on 2016/12/6.
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
+
+gulp.task('server', function () {
+    nodemon({
+        script: 'bin/www',
+        ignore: ["gulpfile.js", "node_modules/", "public/**/*.*"],
+        env: {'NODE_ENV': 'development'}
+    }).on('start', function () {
+        browserSync.init({
+            proxy: 'http://localhost:3000',
+            files: ["public/**/*.*", "views/*.*", "routes/*.*"],
+            browser: ["chrome"],
+            port: 8080
+        }, function () {
+            console.log("browser refreshed.");
+        });
+    });
+});
+
+gulp.task("default", ["server"]);
